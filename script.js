c5178abba638e6a24d4057596a993f90ca97cc8d
Steve1077 <stevevaccio@gmail.com>
2026-05-02 18:13:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ titolo.addEventListener('click', () => {
     titolo.style.color = '#ff4500'; // Diventa arancione al click
     alert('Hai cliccato il titolo! Il JavaScript funziona.');
 });
+let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar-custom');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > lastScrollTop && scrollTop > 100) {
+        // Scroll verso il basso - Nascondi
+        navbar.classList.add('navbar-hidden');
+    } else {
+        // Scroll verso l'alto - Mostra
+        navbar.classList.remove('navbar-hidden');
+    }
+    
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+}, false);
