d4486d9e6618bd823a649b5272e5049a049e3622
Steve1077 <stevevaccio@gmail.com>
2026-05-02 16:56:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Verifica il funzionamento in console
+console.log("Il portfolio è attivo e il JS è collegato correttamente!");
 
+// Esempio dinamico: Cambia il colore del titolo al click
+const titolo = document.querySelector('h1');
+
+titolo.addEventListener('click', () => {
+    titolo.style.color = '#ff4500'; // Diventa arancione al click
+    alert('Hai cliccato il titolo! Il JavaScript funziona.');
+});
